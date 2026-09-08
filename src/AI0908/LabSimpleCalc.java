cb469a14031d2d1265df82c6672739ae17cc6dac
hak801 <bvnyonghag@gmail.com>
2026-09-08 16:30:06 +0900

    10장 메소드 매개변수 반환 오버로딩

---
+++ src/AI0908/LabSimpleCalc.java
@@ -0,0 +1,54 @@
+package AI0908;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class LabSimpleCalc {
+    public static int calc(int n1, int n2, String operator) {
+        int result = 0;
+
+        switch (operator){
+            case "+": result = n1 + n2;
+                        break;
+            case "-": result = n1 - n2;
+                        break;
+            case "*": result = n1 * n2;
+                        break;
+            case "/": result = n1 / n2;
+                        break;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);//정수 입력 받을 때
+        Scanner s2 = new Scanner(System.in);//연산자 기호를 문자열로 입력 받을 때
+
+        System.out.println("사칙연산 계산기");
+
+        while (true) {
+            System.out.print("1. 정수입력:");
+            int n1 = s1.nextInt();
+
+            System.out.print("2. 정수입력:");
+            int n2 = s1.nextInt();
+
+            System.out.println("프로그램 종료를 원하시면 End를 입력하세요");
+            System.out.print("3. 연산자 입력(+, -, *, /):");
+            String operator = s2.nextLine();
+            String nothingCase = operator.toLowerCase();
+
+            if (nothingCase.equals("end")){
+                s1.close();
+                s2.close();
+                return;
+            }
+
+            int result = calc(n1, n2, operator);
+
+            System.out.printf("%d %s %d = %d\n", n1, operator, n2, result);
+        }
+
+    }
+}
